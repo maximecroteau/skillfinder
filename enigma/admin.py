26407f093a8fbf2387494b842ae7a5f5ab791af5
maximecroteau <maxime.croteau85@gmail.com>
2019-11-27 15:21:42 +0100

    change model

--- enigma/admin.py
+++ enigma/admin.py
@@ -1,20 +1,14 @@
 from django.contrib import admin
-from .models import Results, Answers
+from .models import Results
 # Register your models here.
 from django.contrib.sessions.models import Session
 
 
 class ResultsAdmin(admin.ModelAdmin):
-    list_display = ('time', 'score', 'firstname', 'lastname', 'mail', 't_answer1_1', 'tentative1_1', 't_answer1_2', 'tentative1_2', 't_answer1_3',
-                    't_answer2_1', 'tentative2_1', 't_answer2_2', 'tentative2_2', 't_answer2_3', 'tentative2_3', 't_answer3_1', 'tentative3_1', 't_answer3_2', 'tentative3_2', 't_answer3_3', 'tentative3_3')
+    list_display = ('time', 'score', 'firstname', 'lastname', 'mail', 't_answer1_1', 'tentative1_1', 'score1_1', 't_answer1_2', 'tentative1_2', 'score1_2', 't_answer1_3', 'tentative1_3', 'score1_3',
+                    't_answer2_1', 'tentative2_1', 'score2_1', 't_answer2_2', 'tentative2_2', 'score2_2', 't_answer2_3', 'tentative2_3', 'score2_3', 't_answer3_1', 'tentative3_1', 'score3_1', 't_answer3_2', 'tentative3_2', 'score3_2', 't_answer3_3', 'tentative3_3', 'score3_3')
     list_filter = ('time', 'score', 'firstname', 'lastname', 'mail')
 
 
-class AnswersAdmin(admin.ModelAdmin):
-    list_display = ('answer1_1', 'answer1_2', 'answer1_3', 'answer2_1', 'answer2_2', 'answer2_3', 'answer3_1',
-                    'answer3_2', 'answer3_3',)
-
-
 admin.site.register(Results, ResultsAdmin)
-admin.site.register(Answers, AnswersAdmin)
 admin.site.register(Session)
--- enigma/models.py
+++ enigma/models.py
@@ -9,39 +9,42 @@ class Results(models.Model):
     firstname = models.CharField(max_length=30)
     lastname = models.CharField(max_length=30)
     mail = models.EmailField()
+
     t_answer1_1 = models.IntegerField()
     tentative1_1 = models.IntegerField(default=0)
+    score1_1 = models.FloatField(default=0)
+
     t_answer1_2 = models.IntegerField()
     tentative1_2 = models.IntegerField(default=0)
+    score1_2 = models.FloatField(default=0)
+
     t_answer1_3 = models.IntegerField()
     tentative1_3 = models.IntegerField(default=0)
+    score1_3 = models.FloatField(default=0)
+
     t_answer2_1 = models.IntegerField()
     tentative2_1 = models.IntegerField(default=0)
+    score2_1 = models.FloatField(default=0)
+
     t_answer2_2 = models.IntegerField()
     tentative2_2 = models.IntegerField(default=0)
+    score2_2 = models.FloatField(default=0)
+
     t_answer2_3 = models.IntegerField()
     tentative2_3 = models.IntegerField(default=0)
+    score2_3 = models.FloatField(default=0)
+
     t_answer3_1 = models.IntegerField()
     tentative3_1 = models.IntegerField(default=0)
+    score3_1 = models.FloatField(default=0)
+
     t_answer3_2 = models.IntegerField()
     tentative3_2 = models.IntegerField(default=0)
+    score3_2 = models.FloatField(default=0)
+
     t_answer3_3 = models.IntegerField()
     tentative3_3 = models.IntegerField(default=0)
+    score3_3 = models.FloatField(default=0)
 
     class Meta:
         verbose_name = "Resultat"
-
-
-class Answers(models.Model):
-    answer1_1 = models.CharField(max_length=50, default="3")
-    answer1_2 = models.CharField(max_length=50, default="22")
-    answer1_3 = models.CharField(max_length=50, default="open")
-    answer2_1 = models.CharField(max_length=50, default="rouge")
-    answer2_2 = models.CharField(max_length=50, default="vermeil")
-    answer2_3 = models.CharField(max_length=50, default="herodote")
-    answer3_1 = models.CharField(max_length=50, default="secret")
-    answer3_2 = models.CharField(max_length=50, default="virgule")
-    answer3_3 = models.CharField(max_length=50, default="rdvnatio")
-
-    class Meta:
-        verbose_name = "Reponse"
